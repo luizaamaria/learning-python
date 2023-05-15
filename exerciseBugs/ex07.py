6b6bb1706a56e8f54f730b2b15c8ea5f19e1671f
luizaamaria <luizamariabarros.contato@gmail.com>
2023-05-15 15:40:30 -0300

    bug fixing

---
+++ exerciseBugs/ex07.py
@@ -0,0 +1,25 @@
+'''
+
+A programmer wrote the following program:
+
+countries = []
+
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+print(countries)
+The expected output is as follows:
+
+
+However, the code returns an error instead of the expected output.
+Fix the code, so it produces the expected output.
+
+'''
+
+countries = []
+
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+
+    print(countries)
